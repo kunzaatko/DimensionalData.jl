e81fe31e68aa789b0a807232375fc0bd21dd5ad7
Documenter.jl <documenter@juliadocs.github.io>
2024-02-17 19:02:44 +0000

    build based on 8fa4c8f3

---
+++ previews/PR632/assets/plots.md.FCVFwOnp.lean.js
@@ -0,0 +1,1 @@
+import{_ as A,c as C,o as Q,V as E}from"./chunks/framework.b1Lc_bjX.js";const K=JSON.parse('{"title":"Plots.jl","description":"","frontmatter":{},"headers":[],"relativePath":"plots.md","filePath":"plots.md","lastUpdated":null}'),I={name:"plots.md"},g=E("",13),h=[g];function B(s,t,e,i,l,U){return Q(),C("div",null,h)}const v=A(I,[["render",B]]);export{K as __pageData,v as default};
