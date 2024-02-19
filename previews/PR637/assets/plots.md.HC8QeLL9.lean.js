532d7a57a97724a8c21f4e620030a1104682c197
Documenter.jl <documenter@juliadocs.github.io>
2024-02-19 00:56:18 +0000

    build based on bef9f619

---
+++ previews/PR637/assets/plots.md.HC8QeLL9.lean.js
@@ -0,0 +1,1 @@
+import{_ as A,c as C,o as Q,V as E}from"./chunks/framework.FQXvoMmx.js";const a=JSON.parse('{"title":"Plots.jl","description":"","frontmatter":{},"headers":[],"relativePath":"plots.md","filePath":"plots.md","lastUpdated":null}'),I={name:"plots.md"},g=E("",13),h=[g];function B(s,e,l,o,U,i){return Q(),C("div",null,h)}const d=A(I,[["render",B]]);export{a as __pageData,d as default};
--- previews/PR637/assets/plots.md.ni_Dnc-D.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as A,c as C,o as Q,V as E}from"./chunks/framework.TA0Qxhdf.js";const e=JSON.parse('{"title":"Plots.jl","description":"","frontmatter":{},"headers":[],"relativePath":"plots.md","filePath":"plots.md","lastUpdated":null}'),I={name:"plots.md"},g=E("",13),B=[g];function h(o,s,i,U,l,a){return Q(),C("div",null,B)}const t=A(I,[["render",h]]);export{e as __pageData,t as default};
