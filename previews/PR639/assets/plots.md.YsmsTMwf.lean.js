4f9c68b94d21889d75f02b053f27e29f2cfb2246
Documenter.jl <documenter@juliadocs.github.io>
2024-02-20 13:44:34 +0000

    build based on 2806d336

---
+++ previews/PR639/assets/plots.md.YsmsTMwf.lean.js
@@ -0,0 +1,1 @@
+import{_ as A,c as C,o as Q,V as E}from"./chunks/framework._2zkOB8h.js";const a=JSON.parse('{"title":"Plots.jl","description":"","frontmatter":{},"headers":[],"relativePath":"plots.md","filePath":"plots.md","lastUpdated":null}'),I={name:"plots.md"},g=E("",13),B=[g];function h(s,l,o,e,U,i){return Q(),C("div",null,B)}const n=A(I,[["render",h]]);export{a as __pageData,n as default};
