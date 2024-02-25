ba389b31e00ea889735152cfb2d88259a5e2c66f
Documenter.jl <documenter@juliadocs.github.io>
2024-02-25 10:07:23 +0000

    build based on 7662dc9b

---
+++ dev/assets/plots.md.4GH9TbE7.lean.js
@@ -0,0 +1,1 @@
+import{_ as A,c as Q,o as C,V as E}from"./chunks/framework.SSsEDEan.js";const t=JSON.parse('{"title":"Plots.jl","description":"","frontmatter":{},"headers":[],"relativePath":"plots.md","filePath":"plots.md","lastUpdated":null}'),I={name:"plots.md"},g=E("",13),h=[g];function B(o,i,s,e,a,l){return C(),Q("div",null,h)}const d=A(I,[["render",B]]);export{t as __pageData,d as default};
