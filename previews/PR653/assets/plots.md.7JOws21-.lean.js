d0d299107609307a770366b5f240e879031a9890
Documenter.jl <documenter@juliadocs.github.io>
2024-02-28 22:11:05 +0000

    build based on 09120143

---
+++ previews/PR653/assets/plots.md.7JOws21-.lean.js
@@ -0,0 +1,1 @@
+import{_ as A,c as C,o as Q,V as E}from"./chunks/framework.fF1VwLjv.js";const d=JSON.parse('{"title":"Plots.jl","description":"","frontmatter":{},"headers":[],"relativePath":"plots.md","filePath":"plots.md","lastUpdated":null}'),I={name:"plots.md"},g=E("",13),B=[g];function h(s,e,i,t,l,a){return Q(),C("div",null,B)}const p=A(I,[["render",h]]);export{d as __pageData,p as default};
--- previews/PR653/assets/plots.md.TF0-SJKB.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as A,c as C,o as Q,V as E}from"./chunks/framework.fF1VwLjv.js";const d=JSON.parse('{"title":"Plots.jl","description":"","frontmatter":{},"headers":[],"relativePath":"plots.md","filePath":"plots.md","lastUpdated":null}'),I={name:"plots.md"},g=E("",13),B=[g];function h(i,e,o,s,t,l){return Q(),C("div",null,B)}const n=A(I,[["render",h]]);export{d as __pageData,n as default};
