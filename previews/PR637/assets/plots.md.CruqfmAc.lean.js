45722b6ea14ff1cf821302e2804a14d7e338a635
Documenter.jl <documenter@juliadocs.github.io>
2024-02-29 02:43:24 +0000

    build based on 630bb7e4

---
+++ previews/PR637/assets/plots.md.CruqfmAc.lean.js
@@ -0,0 +1,1 @@
+import{_ as A,c as C,o as Q,V as E}from"./chunks/framework.NFgYKdDm.js";const n=JSON.parse('{"title":"Plots.jl","description":"","frontmatter":{},"headers":[],"relativePath":"plots.md","filePath":"plots.md","lastUpdated":null}'),I={name:"plots.md"},g=E("",13),h=[g];function B(i,s,o,e,l,t){return Q(),C("div",null,h)}const a=A(I,[["render",B]]);export{n as __pageData,a as default};
