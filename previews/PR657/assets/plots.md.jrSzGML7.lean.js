628a69466fca047edcedbbf00b02df9219824776
Documenter.jl <documenter@juliadocs.github.io>
2024-02-29 02:30:21 +0000

    build based on a6c98f24

---
+++ previews/PR657/assets/plots.md.jrSzGML7.lean.js
@@ -0,0 +1,1 @@
+import{_ as A,c as C,o as Q,V as E}from"./chunks/framework.HXXCYdQL.js";const a=JSON.parse('{"title":"Plots.jl","description":"","frontmatter":{},"headers":[],"relativePath":"plots.md","filePath":"plots.md","lastUpdated":null}'),I={name:"plots.md"},g=E("",13),B=[g];function h(s,o,l,K,i,e){return Q(),C("div",null,B)}const n=A(I,[["render",h]]);export{a as __pageData,n as default};
