dbdf7ff8a375805bca9a4af64ee7b354e6693003
Documenter.jl <documenter@juliadocs.github.io>
2024-07-12 11:48:34 +0000

    build based on b5791009

--- dev/assets/basics.md.BFGZ-OyZ.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as s,c as i,o as a,a5 as n}from"./chunks/framework.CbQts6qq.js";const c=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"basics.md","filePath":"basics.md","lastUpdated":null}'),h={name:"basics.md"},l=n("",21),t=[l];function k(e,p,d,r,g,y){return a(),i("div",null,t)}const E=s(h,[["render",k]]);export{c as __pageData,E as default};
