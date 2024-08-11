25b0ff7bfc998e31d19bfb0d2362246f333455e3
Documenter.jl <documenter@juliadocs.github.io>
2024-08-11 20:57:01 +0000

    build based on f6ab4500

---
+++ previews/PR764/assets/basics.md.D18qPBJL.lean.js
@@ -0,0 +1,1 @@
+import{_ as s,c as i,o as a,a6 as n}from"./chunks/framework.BquXndwr.js";const c=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"basics.md","filePath":"basics.md","lastUpdated":null}'),h={name:"basics.md"},e=n("",21),k=[e];function l(t,p,d,r,g,y){return a(),i("div",null,k)}const E=s(h,[["render",l]]);export{c as __pageData,E as default};
